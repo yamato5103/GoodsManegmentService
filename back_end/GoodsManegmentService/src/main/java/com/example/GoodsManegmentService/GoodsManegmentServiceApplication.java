c8d5b32dce51caa56e15c374f9ec62463fe75be1
yamato5103 <sin1062.bad.gmail.com>
2023-06-19 18:04:25 +0900

    first commit

---
+++ back_end/GoodsManegmentService/src/main/java/com/example/GoodsManegmentService/GoodsManegmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoodsManegmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsManegmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsManegmentServiceApplication.class, args);
+	}
+
+}
---
+++ back_end/GoodsManegmentService/src/test/java/com/example/GoodsManegmentService/GoodsManegmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoodsManegmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsManegmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
