fca39a5be085a40627fd8c484f0211804fe563c7
yamato5103 <sin1062.bad.gmail.com>
2023-06-21 18:44:03 +0900

    be_app commit

---
+++ back_end/be_app/src/test/java/com/example/be_app/BeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
