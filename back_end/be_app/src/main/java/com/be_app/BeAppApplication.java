8f0d38a8468d1f226db872f60ad2b1756a81442d
yamato5103 <sin1062.bad.gmail.com>
2023-06-21 19:11:25 +0900

    update

---
+++ back_end/be_app/src/main/java/com/be_app/BeAppApplication.java
@@ -0,0 +1,13 @@
+package com.be_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeAppApplication.class, args);
+	}
+
+}
